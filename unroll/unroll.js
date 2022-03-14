09e263ed53b30e074c123cea900e119726727f3d
Rijo George <rijogeorge@rijos-mbp-2.lan>
2022-03-14 12:49:56 -0700

    unroll function is done

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,35 @@
 function unroll(squareArray) {
+    let left = 0;
+    let top =0;
+    let right = squareArray[0].length;
+    let bottom = squareArray.length;
+    const resultArray = [];
+    while (left < right && top < bottom) {
+        // travel right
+        for(i=left; i<right; i++) {
+            resultArray.push(squareArray[left][i]);
+        }
 
+        // travel top to bottom
+        for(i=top+1; i<bottom; i++) {
+            resultArray.push(squareArray[i][right-1]);
+        }
+
+        // travel bottom to left
+        for(i=right-2; i>=left; i--) {
+            resultArray.push(squareArray[bottom-1][i]);
+        }
+
+        // travel bottom to top 
+        for(i=bottom-2; i>top;i--) {
+            resultArray.push(squareArray[i][left])
+        }
+        left += 1;
+        top += 1;
+        right -= 1;
+        bottom -= 1;
+    }
+    return resultArray;
 }
 
 module.exports = unroll;
--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -6,4 +6,25 @@ describe("#unroll", function () {
     expect(typeof unroll).toEqual("function");
   });
 
+  it("should return", function () {
+    const squareArray = [
+      [1, 2, 3, 4],
+      [5, 6, 7, 8],
+      [9, 10, 11, 12],
+      [13, 14, 15, 16]
+    ];
+    const resultArray = [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10];
+    expect(unroll(squareArray)).toEqual(resultArray);
+  });
+
+  it("should return", function () {
+    const squareArray = [
+      ["a", "b", "c"],
+      ["d", "e", "f"],
+      ["g", "h", "i"]
+    ];
+    const resultArray = ["a", "b", "c", "f", "i", "h", "g", "d", "e"];
+    expect(unroll(squareArray)).toEqual(resultArray);
+  });
+
 });
